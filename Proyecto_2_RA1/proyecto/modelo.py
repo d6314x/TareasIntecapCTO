33be74d3530238a5b4388f11de4cab6cad23c30f
ale-thv <ale57hidalgo@gmail.com>
2025-05-20 14:35:29 -0600

    Update modelo.py

--- Proyecto_2_RA1/proyecto/modelo.py
+++ Proyecto_2_RA1/proyecto/modelo.py
@@ -1,4 +1,8 @@
 # modelo.py
+"""
+El código muestra la creacion de la clase tipo NINO, que nos permite crear objetos 
+para almacenar los datos
+"""
 class Nino:
     _contador_id = 1  # variable de clase (compartida entre todas las instancias)
 
@@ -6,9 +10,9 @@ class Nino:
         self.id = Nino._contador_id  # asigna el ID actual
         Nino._contador_id += 1       # incrementa para el siguiente niño
 
-        self.edad = edad
-        self.peso = peso
-        self.talla = talla
-        self.craneo = craneo
-        self.departamento = departamento
+        self.edad = edad #Indica el registro de la edad
+        self.peso = peso #Registro del peso
+        self.talla = talla #Registra la talla
+        self.craneo = craneo #Registra el perímetro craneal
+        self.departamento = departamento #Registra la cantidad por departamento 
 
