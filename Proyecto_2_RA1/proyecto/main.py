3cd42952a9ec94c9a4ad31308b422c4b0dac572f
ale-thv <ale57hidalgo@gmail.com>
2025-05-20 14:05:27 -0600

    Update main.py

--- Proyecto_2_RA1/proyecto/main.py
+++ Proyecto_2_RA1/proyecto/main.py
@@ -1,5 +1,9 @@
 from menu import ejecutar_menu
-
+#Del archivo menú importar el código ejecutar_menu
+'''
+Este código procesa y registra el ingreso de datos del crecimiento infantil
+según los estándares de la OMS
+'''
 if __name__ == "__main__":
     ejecutar_menu()
 
