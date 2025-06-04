53782eb9fee0931dd3679e77707105488e2ee926
EdwinJuanfernando <edwinjuan1500@gmail.com>
2025-06-04 15:43:49 -0600

    Update proveedores.py
    pele lo que hice

--- CarritoTiendaOnline/proveedores.py
+++ CarritoTiendaOnline/proveedores.py
@@ -25,7 +25,7 @@ class Proveedor:
         print(f"Correo: {self.correo}")
         print(f"Persona de Contacto: {self.contacto}")
 
-
+           #almacena los datos
 class GestorProveedores:
     def __init__(self):
         self.lista_proveedores = []
@@ -34,7 +34,7 @@ class GestorProveedores:
         nuevo = Proveedor(nombre, direccion, telefono, correo, contacto)
         self.lista_proveedores.append(nuevo)
         print(f"\nProveedor '{nombre}' agregado con ID {nuevo.id}.")
-            
+            #mustra el mensaage si no hay datos
     def mostrar_proveedores(self):
         if not self.lista_proveedores:
             print("\nNo hay proveedores registrados.")
@@ -42,7 +42,7 @@ class GestorProveedores:
         print("\nLista de proveedores:")
         for prov in self.lista_proveedores:
             prov.mostrar_informacion()
-
+            #esto deja que el provedor edita las cosas y a la persona agregada
     def editar_proveedor(self, id_proveedor, nuevo_nombre=None, nueva_direccion=None,
                          nuevo_telefono=None, nuevo_correo=None, nuevo_contacto=None):
         for prov in self.lista_proveedores:
@@ -55,7 +55,7 @@ class GestorProveedores:
                 print(f"\nProveedor ID {id_proveedor} ha sido actualizado.")
                 return
         print(f"\nProveedor con ID {id_proveedor} no encontrado.")
-
+              #sirve para elinibar un proveedor
     def eliminar_proveedor(self, id_proveedor):
         for prov in self.lista_proveedores:
             if prov.id == id_proveedor:
